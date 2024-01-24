de456b634fc1d500b3bf438aecddf406ac7ce8db
N0V8rs <noahvatersnscc@gmail.com>
2024-01-24 10:36:05 -0400

    Update Rotate.cs

--- Assets/Rotate.cs
+++ Assets/Rotate.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+    float speed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        object obj = transform.position;
     }
 }
